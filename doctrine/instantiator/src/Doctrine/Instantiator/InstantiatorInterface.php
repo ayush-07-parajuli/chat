f7a8d173d39e9871014a3d093aa03659f9a84726
ayush-07-parajuli <ayushparajuli0707@gmail.com>
2024-08-11 18:52:07 +0545

    Add files via upload

---
+++ doctrine/instantiator/src/Doctrine/Instantiator/InstantiatorInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Doctrine\Instantiator;
+
+use Doctrine\Instantiator\Exception\ExceptionInterface;
+
+/**
+ * Instantiator provides utility methods to build objects without invoking their constructors
+ */
+interface InstantiatorInterface
+{
+    
+    public function instantiate($className);
+}
